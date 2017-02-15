12074584faa5bf4b12f9d149bb0a949ddb613166
Nicolas Ovejero <ovejer_n@epitech.eu>
2017-02-14 20:45:26 -0500

    get file from lorumpixel !

--- Dispatcher.js
+++ Dispatcher.js
@@ -1,13 +1,30 @@
 var eb = vertx.eventBus();
 
-
-// loop get info
-
 // -- generate text with Faker.js
 // -- send paragraphe to Translator
 // -- get image from http://lorempixel.com/800/600/cats/i/
 // -- send images to Resize
 
 vertx.setPeriodic(1000, function (id) {
-    eb.send("translater", "hello world!");
+///    eb.send("translater", "hello world!");
+    getImageFromPixel();
 });
+
+function getRandom() {
+    return Math.floor(Math.random() * 10) + 1;
+}
+
+function getImageFromPixel() {
+    var options = {
+	"defaultHost" : "lorempixel.com"
+    };
+    var client = vertx.createHttpClient(options);
+    client.getNow("/800/600/cats/" + getRandom() + "/", function (response) {
+        console.log(response.statusCode());
+        /*
+        response.bodyHandler(function (body) {
+            eb.publish("");
+        });
+        */
+    });
+}
